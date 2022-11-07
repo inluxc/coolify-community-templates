f12ef1c19c7a48d962855927cbf3c9ebf81babda
Andras Bacsai <andras.bacsai@gmail.com>
2022-11-07 13:45:30 +0100

    fix: minio service fix: use local devTemplates

--- services/scripts/split.mjs
+++ services/scripts/split.mjs
@@ -2,8 +2,15 @@ import fs from 'fs/promises';
 import yaml from 'js-yaml';
 import got from 'got';
 
-const templateDir = 'https://github.com/coollabsio/coolify/raw/next/apps/api/devTemplates.yaml'
-const data = await got.get(templateDir).text()
+async function getRemote() {
+    const templateDir = 'https://github.com/coollabsio/coolify/raw/next/apps/api/devTemplates.yaml'
+    return await got.get(templateDir).text()
+}
+async function getLocal() {
+    const templateDir = '../coolify/apps/api/devTemplates.yaml'
+    return await fs.readFile(templateDir, 'utf8')
+}
+const data = await getLocal();
 const json = yaml.load(data)
 for (const template of json) {
     const type = template.type
